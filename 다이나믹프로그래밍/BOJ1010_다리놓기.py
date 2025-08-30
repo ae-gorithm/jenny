d4e3be2260991a95bc17a12b405aeb5b85fdeead
soonge2 <jiwonsoong13@gmail.com>
2025-08-30 23:53:49 +0900

    [BOJ] 1010번/실버5/DP

--- 다이나믹프로그래밍/BOJ1010_다리놓기.py
+++ 다이나믹프로그래밍/BOJ1010_다리놓기.py
@@ -6,3 +6,18 @@
 -> DP로 푸는 법?
 
 """
+dp = [[0] * 31 for _ in range(31)] # dp[i][j] = i개 중 j개를 고르는 경우의 수
+
+for i in range(31):
+    dp[i][0] = 1     # iC0 = 1
+    dp[i][i] = 1     # iCi = 1
+
+for i in range(1, 31):
+    for j in range(1, i):
+        dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+
+T = int(input())
+
+for _ in range(T):
+    n, m = map(int, input().split())
+    print(dp[m][n])
